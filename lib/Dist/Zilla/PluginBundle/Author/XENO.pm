d1618c9e3edabe42481bf80dfeaa91c97db764cf
Caleb Cushing <xenoterracide@gmail.com>
2014-02-28 15:24:09 -0600

    commit initial authorbundle
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Dist/Zilla/PluginBundle/Author/XENO.pm
+++ lib/Dist/Zilla/PluginBundle/Author/XENO.pm
@@ -1,9 +1,72 @@
 package Dist::Zilla::PluginBundle::Author::XENO;
-use 5.006;
+use 5.008;
 use strict;
 use warnings;
 
 # VERSION
+
+use Moose;
+with qw(
+	Dist::Zilla::Role::PluginBundle::Easy
+);
+
+has install => (
+	isa     => 'Bool',
+	is      => 'ro',
+	lazy    => 1,
+	default => 1,
+);
+
+sub configure {
+	my $self = shift;
+
+	my @plugins = (
+		[ PruneFiles => {
+			filenames => [ qw( dist.ini weaver.ini ) ],
+		}], qw(
+		AutoPrereqs
+		ReadmeFromPod
+		OurPkgVersion
+		PodWeaver
+
+		MetaProvides::Package
+		MetaJSON
+
+		RunExtraTests
+		PodCoverageTests
+		PodSyntaxTests
+		Test::ReportPrereqs
+		Test::Compile
+		Test::EOL
+		Test::Portability
+		Test::Perl::Critic
+
+		ContributorsFromGit
+	));
+
+	if ( $ENV{RELEASE_TESTING} ) {
+		push @plugins, (qw(
+		Test::UnusedVars
+		Test::CPAN::Meta::JSON
+		Test::DistManifest
+		Test::Version
+		Test::CPAN::Changes
+		Test::MinimumVersion
+
+		Git::Remote::Check
+		CheckChangesHasContent
+		));
+
+		push @plugins, (
+			[ 'InstallRelease' => { install_command => "cpanm ." } ],
+		) if $self->install;
+
+# must be last
+		push @plugins, ('Clean'),
+	}
+
+	$self->add_plugins( @plugins );
+}
 
 1;
 
