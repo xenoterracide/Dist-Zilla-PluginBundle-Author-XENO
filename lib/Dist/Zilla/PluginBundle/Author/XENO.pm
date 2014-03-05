f78ad68bae080732f79180c59d2e5e064e0d4e15
Caleb Cushing <xenoterracide@gmail.com>
2014-03-04 21:04:21 -0600

    Revert "mention installing with recommends"
    This reverts commit 049d2d674634668f82047df7f0481f8ab43ddab3.

--- lib/Dist/Zilla/PluginBundle/Author/XENO.pm
+++ lib/Dist/Zilla/PluginBundle/Author/XENO.pm
@@ -80,10 +80,4 @@ in C<dist.ini>
 	[@Author::XENO]
 	install = 0 ; optional, disables InstallRelease
 
-=head1 INSTALL
-
-This will get you everything
-
-   cpanm --with-recommends Dist::Zilla::PluginBundle::Author::XENO
-
 =method configure
